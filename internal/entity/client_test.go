a45f4960a109e26d0377c8105004aeea2c4e37d9
silva4dev <web4dev.lucas@outlook.com>
2024-04-20 22:48:58 -0300

    test: add client entity

---
+++ internal/entity/client_test.go
@@ -0,0 +1,21 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewClient(t *testing.T) {
+	client, err := NewClient("John Doe", "q5GQk@example.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "q5GQk@example.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
