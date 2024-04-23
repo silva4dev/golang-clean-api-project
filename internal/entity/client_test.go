d0cd7d32027853019a1966682b9584ba5e1994c8
silva4dev <web4dev.lucas@outlook.com>
2024-04-23 11:12:21 -0300

    test: add account to client

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -33,3 +33,11 @@ func TestUpdateClientWithInvalidArgs(t *testing.T) {
 	err := client.Update("", "j@j.com")
 	assert.NotNil(t, err, "name is required")
 }
+
+func TestAddAccountToClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	err := client.AddAccount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
