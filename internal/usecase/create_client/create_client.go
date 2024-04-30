e37959b6bb92ba4038a64372fc65cdd2b17579cf
silva4dev <web4dev.lucas@outlook.com>
2024-04-30 13:45:24 -0300

    feat: add create client use case

---
+++ internal/usecase/create_client/create_client.go
@@ -0,0 +1,48 @@
+package createclient
+
+import (
+	"github/silva4dev/golang-ms-wallet-project/internal/entity"
+	"github/silva4dev/golang-ms-wallet-project/internal/gateway"
+	"time"
+)
+
+type CreateClientDTO struct {
+	Name  string
+	Email string
+}
+
+type CreateClientOutputDTO struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CreateClientUseCase struct {
+	ClientGateway gateway.ClientGateway
+}
+
+func NewCreateClientUseCase(clientGateway gateway.ClientGateway) *CreateClientUseCase {
+	return &CreateClientUseCase{
+		ClientGateway: clientGateway,
+	}
+}
+
+func (uc *CreateClientUseCase) Execute(input CreateClientDTO) (*CreateClientOutputDTO, error) {
+	client, err := entity.NewClient(input.Name, input.Email)
+	if err != nil {
+		return nil, err
+	}
+	err = uc.ClientGateway.Save(client)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateClientOutputDTO{
+		ID:        client.ID,
+		Name:      client.Name,
+		Email:     client.Email,
+		CreatedAt: client.CreatedAt,
+		UpdatedAt: client.UpdatedAt,
+	}, nil
+}
