4954af07d013636e3f73c0a3954b3a5502ab97f5
silva4dev <web4dev.lucas@outlook.com>
2024-05-02 21:31:14 -0300

    refactor: move mocks to account

--- internal/usecase/create_account/create_account_test.go
+++ internal/usecase/create_account/create_account_test.go
@@ -2,55 +2,31 @@ package createaccount
 
 import (
 	"github/silva4dev/golang-ms-wallet-project/internal/entity"
+	"github/silva4dev/golang-ms-wallet-project/internal/gateway/mocks"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type ClientGatewayMock struct {
-	mock.Mock
-}
-
-func (m *ClientGatewayMock) Save(client *entity.Client) error {
-	args := m.Called(client)
-	return args.Error(0)
-}
-
-func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Client), args.Error(1)
-}
-
-type AccountGatewayMock struct {
-	mock.Mock
-}
-
-func (m *AccountGatewayMock) Save(account *entity.Account) error {
-	args := m.Called(account)
-	return args.Error(0)
-}
-
-func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Account), args.Error(1)
-}
-
 func TestCreateAccountUseCase_Execute(t *testing.T) {
-	client, _ := entity.NewClient("John Doe", "j@j")
-	clientMock := &ClientGatewayMock{}
-	clientMock.On("Get", client.ID).Return(client, nil)
-	accountMock := &AccountGatewayMock{}
-	accountMock.On("Save", mock.Anything).Return(nil)
-	uc := NewCreateAccountUseCase(accountMock, clientMock)
+	client, _ := entity.NewClient("John Doe", "john@doe.com")
+	clientGatewayMock := mocks.NewClientGatewayMock()
+	clientGatewayMock.On("Get", client.ID).Return(client, nil)
+
+	accountGatewayMock := mocks.NewAccountGatewayMock()
+	accountGatewayMock.On("Save", mock.Anything).Return(nil)
+
+	uc := NewCreateAccountUseCase(accountGatewayMock, clientGatewayMock)
 	inputDto := CreateAccountInputDTO{
 		ClientID: client.ID,
 	}
+
 	output, err := uc.Execute(inputDto)
 	assert.Nil(t, err)
 	assert.NotNil(t, output.ID)
-	clientMock.AssertExpectations(t)
-	accountMock.AssertExpectations(t)
-	clientMock.AssertNumberOfCalls(t, "Get", 1)
-	accountMock.AssertNumberOfCalls(t, "Save", 1)
+	clientGatewayMock.AssertExpectations(t)
+	accountGatewayMock.AssertExpectations(t)
+	clientGatewayMock.AssertNumberOfCalls(t, "Get", 1)
+	accountGatewayMock.AssertNumberOfCalls(t, "Save", 1)
 }
