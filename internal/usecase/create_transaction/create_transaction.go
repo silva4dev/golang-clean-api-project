cd01ac2f5fed7ec22a36aa7f8859dc4d1df1fab1
silva4dev <web4dev.lucas@outlook.com>
2024-05-02 21:32:15 -0300

    feat: add create transaction use case

---
+++ internal/usecase/create_transaction/create_transaction.go
@@ -0,0 +1,54 @@
+package createtransaction
+
+import (
+	"github/silva4dev/golang-ms-wallet-project/internal/entity"
+	"github/silva4dev/golang-ms-wallet-project/internal/gateway"
+)
+
+type CreateTransactionInputDTO struct {
+	AccountIDFrom string
+	AccountIDTo   string
+	Amount        float64
+}
+
+type CreateTransactionOutputDTO struct {
+	ID string
+}
+
+type CreateTransactionUseCase struct {
+	TransactionGateway gateway.TransactionGateway
+	AccountGateway     gateway.AccountGateway
+}
+
+func NewCreateTransactionUseCase(transactionGateway gateway.TransactionGateway, accountGateway gateway.AccountGateway) *CreateTransactionUseCase {
+	return &CreateTransactionUseCase{
+		TransactionGateway: transactionGateway,
+		AccountGateway:     accountGateway,
+	}
+}
+
+func (useCase *CreateTransactionUseCase) Execute(input CreateTransactionInputDTO) (*CreateTransactionOutputDTO, error) {
+	accountFrom, err := useCase.AccountGateway.FindByID(input.AccountIDFrom)
+	if err != nil {
+		return nil, err
+	}
+
+	accountTo, err := useCase.AccountGateway.FindByID(input.AccountIDTo)
+	if err != nil {
+		return nil, err
+	}
+
+	transaction, err := entity.NewTransaction(accountFrom, accountTo, input.Amount)
+	if err != nil {
+		return nil, err
+	}
+
+	err = useCase.TransactionGateway.Create(transaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateTransactionOutputDTO{
+		ID: transaction.ID,
+	}, nil
+}
